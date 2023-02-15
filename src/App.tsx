80f6670917e58d8820bacf560b3b423e88ddf86c
Infinity <2poh.junkang@gmail.com>
2023-02-15 17:36:54 +0800

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,12 @@ import ContactMe from "./components/contact/contact";
 function App() {
     return (
         <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home page="1" />} />
-                    <Route path="/timeline" element={<Timeline page="2" />} />
-                    <Route path="/about" element={<About page="3" />} />
-                    <Route path="/contact" element={<ContactMe page="4" />} />
-                </Routes>
-            </Router>
+            <Routes>
+                <Route path="/" element={<Home page="1" />} />
+                <Route path="/timeline" element={<Timeline page="2" />} />
+                <Route path="/about" element={<About page="3" />} />
+                <Route path="/contact" element={<ContactMe page="4" />} />
+            </Routes>
         </>
     );
 }
