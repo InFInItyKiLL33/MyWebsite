6c54a0aec4e300e4fb133d5eacdc1b71667bff7d
Infinity <2poh.junkang@gmail.com>
2023-02-15 14:55:12 +0800

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import ContactMe from "./components/contact/contact";
 function App() {
     return (
         <>
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<Home page="1" />} />
                     <Route path="/timeline" element={<Timeline page="2" />} />
