d1322eed0de4d06683a9d5cff78a25a06c5ae1d3
Infinity <2poh.junkang@gmail.com>
2023-07-09 23:50:26 +0800

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,7 @@ import axios from 'axios';
 
 function App() {
 
-    // const DEVMODE = window.location.href.startsWith("http://localhost");
-    const DEVMODE = window.location.href.startsWith("http://localhost") != true;
-    const BACKENDURL = DEVMODE === true ? "http://127.0.0.1:8000/" : "https://infinitytwoproxy.onrender.com/";
+    const BACKENDURL = "https://infinitytwoproxy.onrender.com/";
     const types = {0: "Programming", 1: "Aviation", 2: "PC Building", 3: "Design"};
     const ERR_RETRIES = 10;
     const [searchParams, setSearchParams]:any = useSearchParams(); // search params on url
